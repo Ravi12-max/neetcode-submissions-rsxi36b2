e3cb8225698130f873b9b14b71c0f52c1b13f50d
Ravi Ranjan kumar <ankitku.arayadav@gmail.com>
2026-04-07 05:31:20 +0530

    Add: search-2d-matrix - submission-1

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-1.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        Set<Integer> set = new HashSet<>();
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[0].length;j++){
+                set.add(matrix[i][j]);
+            }
+        }
+        if(set.contains(target)) return true;
+       else return false;
+    }
+}
